6da533432a18279a4433afe7b1935f55db54ee0b
淸梣ling <76830236+qingchenling@users.noreply.github.com>
2022-10-13 14:49:17 +0800

    update v2.1

--- Clap.cpp
+++ Clap.cpp
@@ -1,90 +1,108 @@
-#include <atomic>
-#include<bits/stdc++.h>
-#include <exception>
+#include <iostream>
 #include <mutex>
-#include <pthread.h>
+#include <atomic>
 #include <thread>
 #include <windows.h>
 #include <io.h>
 using namespace std;
 
-const int TIMES = 32; // ×Ü²âÊÔ´ÎÊý
-const int THREADS = 1; // ½ø³ÌÊý
+const int TIMES = 32; // æ€»æµ‹è¯•æ¬¡æ•°
+const int THREADS = 1; // è¿›ç¨‹æ•°
 
 thread task[THREADS];
 float bf=0,ans=0;
 float bfmax,ansmax;
-atomic_int num;
+int num;
 mutex mut;
+
+void color(int a);
+PCWSTR toPCWSTR(const char *str);
+PCWSTR toPCWSTR(string str);
+void work(string s, int t);
+
+int main()
+{
+    SetConsoleOutputCP(65001);
+    int eachTimes=TIMES/THREADS;
+
+    puts("ç¼–è¯‘ä¸­...");
+    if(access("tmp", 0)==-1) mkdir("tmp");
+    system("g++ Data.cpp -std=c++11 -Wl,--stack=2147483647 -O2 -o tmp/Data.exe");
+    system("g++ Std.cpp -std=c++11 -Wl,--stack=2147483647 -O2 -o tmp/Std.exe");
+    system("g++ BF.cpp -std=c++11 -Wl,--stack=2147483647 -O2 -o tmp/BF.exe");
+
+    for(int i=0; i<THREADS; i++)
+    {
+        char tmp[10];
+        sprintf(tmp, "%d", i+1);
+        string src="tmp\\Thread"; src=src+tmp+"\\";
+
+        if(access(src.data(), 0)==-1) mkdir(src.data());
+        CopyFile2(toPCWSTR("tmp/Data.exe"), toPCWSTR(src+"Data.exe"), 0);
+        CopyFile2(toPCWSTR("tmp/Std.exe"), toPCWSTR(src+"Std.exe"), 0);
+        CopyFile2(toPCWSTR("tmp/BF.exe"), toPCWSTR(src+"BF.exe"), 0);
+
+        int q=(i==THREADS-1 ? TIMES-eachTimes*(THREADS-1) : eachTimes);
+        task[i]=thread(work, src, q);
+    }
+    
+    for(auto& th : task)
+    th.join();
+
+    printf("æš´åŠ›å¹³å‡æ—¶é—´: %.3f, æœ€é•¿æ—¶é—´: %.3f\n", bf/TIMES, bfmax);
+    printf("æ­£è§£å¹³å‡æ—¶é—´: %.3f, æœ€é•¿æ—¶é—´: %.3f\n", ans/TIMES, ansmax);
+    system("rmdir /S /Q tmp");
+    return 0;
+}
+
+void color(int a) { SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),a); }
+
+PCWSTR toPCWSTR(const char *str)
+{
+	WCHAR *res = new WCHAR[256];
+	MultiByteToWideChar(CP_ACP, 0, str,strlen(str)+1, res, 256);
+	return res;
+}
+
+PCWSTR toPCWSTR(string str) { return toPCWSTR(str.data()); }
 
 void work(string s, int t)
 {
-	int a,b,c;
+    int a,b,c;
 
-	string DATA=s+"Data.exe", BF=s+"BF.exe", STD=s+"Std.exe";
-	string In=s+"in.txt",OUT1=s+"bf.txt",OUT2=s+"ans.txt";
-	string ord1=DATA+" >"+In;
-	string ord2=BF+" <"+In+" >"+OUT1;
-	string ord3=STD+" <"+In+" >"+OUT2;
-	string ord4=string("fc ")+OUT1+" "+OUT2+" >nul";
-	for(int i=1; i<=t; i++)
-	{
-		system(ord1.data()); a=clock();
-		system(ord2.data()); b=clock();
-		system(ord3.data()); c=clock();
-		
-		mut.lock();
-		printf("Case %d:\n", ++num);
-		if(system(ord4.data())!=0)
-		{
-			cerr<<"¶Ô±È²îÒì\n";
-			CopyFile(In.data(), "data.txt", 0);
-			CopyFile(OUT1.data(), "bf.txt", 0);
-			CopyFile(OUT2.data(), "ans.txt", 0);
-			system("rmdir /S /Q tmp");
-			exit(0);
-		}
-		puts("ÎÞ²îÒì");
-		printf("ÆÓËØÓÃÊ±: %.3fÃë\n", (float)(b-a)/1000);
-		bf+=(float)(b-a)/1000;
-		bfmax=max(bfmax, (float)(b-a)/1000);
-		printf("Õý½âÓÃÊ±: %.3fÃë\n", (float)(c-b)/1000);
-		ans+=(float)(c-b)/1000;
-		ansmax=max(ansmax, (float)(c-b)/1000);
-		puts("*******************************");
-		mut.unlock();
-	}
-}
-int main()
-{
-	int t=TIMES/THREADS;
-
-	puts("±àÒëÖÐ...");
-	if(access("tmp", 0)==-1) mkdir("tmp");
-	system("g++ Data.cpp -std=c++11 -Wl,--stack=2147483647 -O2 -o tmp/Data.exe");
-	system("g++ Std.cpp -std=c++11 -Wl,--stack=2147483647 -O2 -o tmp/Std.exe");
-	system("g++ BF.cpp -std=c++11 -Wl,--stack=2147483647 -O2 -o tmp/BF.exe");
-
-	for(int i=0; i<THREADS; i++)
-	{
-		char tmp[10];
-		sprintf(tmp, "%d", i+1);
-		string src="tmp\\Thread"; src=src+tmp+"\\";
-
-		if(access(src.data(), 0)==-1) mkdir(src.data());
-		CopyFile("tmp/Data.exe", (src+"Data.exe").data(), 0);
-		CopyFile("tmp/Std.exe", (src+"Std.exe").data(), 0);
-		CopyFile("tmp/BF.exe", (src+"BF.exe").data(), 0);
-
-		int q=(i==THREADS-1 ? TIMES-t*(THREADS-1) : t);
-		task[i]=thread(work, src, q);
-	}
-	
-	for(auto& th : task)
-	th.join();
-
-	printf("±©Á¦Æ½¾ùÊ±¼ä: %.3f, ×î³¤Ê±¼ä: %.3f\n", bf/TIMES, bfmax);
-	printf("Õý½âÆ½¾ùÊ±¼ä: %.3f, ×î³¤Ê±¼ä: %.3f\n", ans/TIMES, ansmax);
-	system("rmdir /S /Q tmp");
-	return 0;
+    string DATA=s+"Data.exe", BF=s+"BF.exe", STD=s+"Std.exe";
+    string In=s+"in.txt",OUT1=s+"bf.txt",OUT2=s+"ans.txt";
+    string ord1=DATA+" >"+In;
+    string ord2=BF+" <"+In+" >"+OUT1;
+    string ord3=STD+" <"+In+" >"+OUT2;
+    string ord4=string("fc ")+OUT1+" "+OUT2+" >nul";
+    for(int i=1; i<=t; i++)
+    {
+        system(ord1.data()); a=clock();
+        system(ord2.data()); b=clock();
+        system(ord3.data()); c=clock();
+        
+        mut.lock();
+        printf("Case %d:\n", ++num);
+        if(system(ord4.data())!=0)
+        {
+            color(4);
+            cerr<<"å¯¹æ¯”å·®å¼‚\n";
+            CopyFile2(toPCWSTR(In), toPCWSTR("data.txt"), 0);
+            CopyFile2(toPCWSTR(OUT1), toPCWSTR("bf.txt"), 0);
+            CopyFile2(toPCWSTR(OUT2), toPCWSTR("ans.txt"), 0);
+            system("rmdir /S /Q tmp");
+            color(7);
+            exit(0);
+        }
+        color(2); puts("æ— å·®å¼‚"); color(7);
+        printf("æœ´ç´ ç”¨æ—¶: %.3fç§’\n", (float)(b-a)/1000);
+        bf+=(float)(b-a)/1000;
+        bfmax=max(bfmax, (float)(b-a)/1000);
+        printf("æ­£è§£ç”¨æ—¶: %.3fç§’\n", (float)(c-b)/1000);
+        ans+=(float)(c-b)/1000;
+        ansmax=max(ansmax, (float)(c-b)/1000);
+        puts("*******************************");
+        mut.unlock();
+    }
 }
